d56edf65c1ebc3f38d8c4285412651871b2418c8
AdriaVH <adriavargashernandez@gmail.com>
2024-06-18 10:33:37 +0200

    base hecha

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1 class=" text-emerald-500">
+      Hola, mersi Irene
+    </h1>
+  </div>
+</template>
+<script>
+export default {
+  
+}
+</script>
+<style>
+  
+</style>
